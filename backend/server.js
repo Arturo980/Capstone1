74556098f708b52748537e169eb752066a9e3687
Arturo Figueroa <111364051+Arturo980@users.noreply.github.com>
2025-06-25 02:11:39 +0000

    :(

--- backend/server.js
+++ backend/server.js
@@ -328,7 +328,7 @@ app.delete('/catalog/workers/:id', authenticateToken, isAdmin, async (req, res) 
   } catch (e) { res.status(400).json({ message: e.message }); }
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
