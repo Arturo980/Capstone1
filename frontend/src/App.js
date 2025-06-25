49cc7e98120a49036facb87dc1eceb4fc6f122c5
Arturo Figueroa <111364051+Arturo980@users.noreply.github.com>
2025-06-25 01:33:09 +0000

    Extra

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,8 @@ const initialTeamRow = {
   horaFin: ''
 };
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+
+const API_URL = 'https://capstone-3p87.onrender.com';
 
 const App = () => {
   const [username, setUsername] = useState('');
